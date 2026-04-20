65bd6e5a7e97efcd2587f3ea7d185f17d956af95
edutecni <edutecni@gmail.com>
2026-04-20 16:28:21 -0300

    Ok funcionou 4

--- GravaAudioYoutube/obj/Debug/net6.0/GravaAudioYoutube.AssemblyInfo.cs
+++ GravaAudioYoutube/obj/Debug/net6.0/GravaAudioYoutube.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GravaAudioYoutube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c203546fd65f053f459a2ce428da80572186f7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d732fa29a91112d1027a0d4c4e2ba5e99ddd8795")]
 [assembly: System.Reflection.AssemblyProductAttribute("GravaAudioYoutube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GravaAudioYoutube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
